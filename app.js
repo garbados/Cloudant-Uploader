10759afdebd7893b628ccd22c0e7642e9a43486b
Max Thayer <garbados@gmail.com>
2013-08-28 13:47:48 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+/*
+  Given a CSV file, upload in batches to a Cloudant database.
+*/
+
+var config = require('./config.json'),
+    nano = require('nano'),
+    fs = require('fs'),
+    util = require('./util'),
+    async = require('async'),
+    _ = require('underscore');
+
+function Uploader(opts){
+  var conn = nano([
+        "http://",
+        opts.api_key,
+        ":",
+        opts.api_secret,
+        "@",
+        opts.user,
+        ".cloudant.com"
+      ].join('')),
+      db = conn.use(opts.db),
+      chunk_size = opts.chunk_size;
+
+  // returns a job for async.js
+  // to upload the given docs as a group
+  function batch(docs){
+    return function(done){
+      db.bulk({docs: docs}, done);
+    };
+  }
+
+  // chunk documents into batches
+  function chunk(docs){
+    var i, j, temparray, chunks = [];
+    for (i=0, j=docs.length; i < j; i += chunk_size) {
+        temparray = docs.slice(i, i + chunk_size);
+        chunks.push(temparray);
+    }
+    return chunks;
+  }
+
+  return {
+    batch: batch,
+    chunk: chunk
+  };
+}
+
+function upload(opts){
+  var uploader = Uploader(opts);
+  fs.readFile(config.csv, function(err, data){
+    var docs = util.CSVToJSON(data.toString()),
+        chunks = uploader.chunk(docs),
+        jobs = chunks.map(function(chunk){
+          return uploader.batch(chunk);
+        });
+
+    // run all our upload batches in parallel
+    async.parallel(jobs, function(err, results){
+      if (err) {
+        console.log(err);
+        throw new Error(err);
+      } else {
+        // get any docs we couldn't upload
+        var problem_docs = _.flatten(results.map(function(result){
+          return result.filter(function(doc){
+            if(doc.error){
+              return doc;
+            }
+          });
+        }));
+        // if any docs had problems, throw a tantrum
+        if (problem_docs.length > 0) {
+          console.log(problem_docs);
+          throw new Error("Some docs had problems :(");
+        } else {
+          console.log("Uploaded " + _.flatten(results).length + " docs.");
+        }
+      }
+    });
+  });
+}
+
+upload(config);
